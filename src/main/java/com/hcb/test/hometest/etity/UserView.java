10c5cbcc489d5f97193d106b27bb6810aac1dce9
Zalyalov-Almaz <zalyalov.almaz@gmail.com>
2022-01-06 14:23:56 +0300

    done

--- src/main/java/com/hcb/test/hometest/etity/UserView.java
+++ src/main/java/com/hcb/test/hometest/etity/UserView.java
@@ -2,14 +2,14 @@ package com.hcb.test.hometest.etity;
 
 import lombok.Getter;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "v_user_view")
 @Immutable
+@Subselect("SELECT * FROM v_user_view")
 @Getter
 public class UserView {
 
